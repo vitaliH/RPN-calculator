7823d512388297189c134616ad9d5505a13aec6f
Horban <vitalii.horban@globaltechmakers.com>
2020-04-15 18:41:31 +0300

    fixed rounding bug

--- src/main/java/com/rpn/operation/Division.java
+++ src/main/java/com/rpn/operation/Division.java
@@ -1,11 +1,12 @@
 package com.rpn.operation;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Division implements Operation {
 
     @Override
     public BigDecimal execute(BigDecimal firstValue, BigDecimal secondValue) {
-        return firstValue.divide(secondValue);
+        return firstValue.divide(secondValue, 2, RoundingMode.HALF_UP);
     }
 }
